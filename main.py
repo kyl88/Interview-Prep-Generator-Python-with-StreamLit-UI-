23515e409409bb9db8a04a7b4db6695e15bd82a1
Kyle Padayachee <kyle44987@gmail.com>
2023-11-23 14:20:23 +0200

    Adding sidebar and text area components to interface

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import langchain as lch
 import streamlit as st
 
-st.title("Interview question generator")
+st.title("PREP INTERVIEW  GENERATOR")
+
+interview_question = st.sidebar.selectbox(
+    "Enter your interview question here", ("Tell me more about yourself?"
+                                          ,"What are your strengths"))
+
+if interview_question == "Tell me more about yourself?":
+    st.sidebar.text_area(label ="Enter a valid response",max_chars=300)
