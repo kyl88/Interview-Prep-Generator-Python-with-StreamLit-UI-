5850d7273dbfd981dab90810a322a5efb11f8e81
Kyle Padayachee <kyle44987@gmail.com>
2023-11-23 12:38:12 +0200

    added response to name_chain

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def generate_interview_questions():
+def generate_interview_questions(interview_question):
    llm = openai(temperature = 0.7)
 
    prompt_template_name = PromptTemplate(
       
       input_variables=['interview_question'],
-      template="Suggest a possible interview questions for data analytics."
+      template="Suggest a possible {interview_question} for data analytics."
    )
 
    name_chain = LLMChain(llm=llm,prompt =  prompt_template_name)
-
-   return name_chain
+   
+   response = name_chain({'interview_question':interview_question })
+   return response
 
 if __name__ == "_main_":
-   print(generate_interview_questions())  
+   print(generate_interview_questions("Tell me more about yourself"))  
